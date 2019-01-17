d7b013fdc04b3c2f64b869a693d866af1519585e
Miff Falden <miff@miffthefox.info>
2019-01-17 16:43:50 -0500

    why did i make this?

---
+++ octo.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var audio = new AudioContext();
+
+function beep(freq){
+    var oscillator = audio.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.setValueAtTime(freq, audio.currentTime);
+    oscillator.connect(audio.destination);
+    oscillator.start();
+    window.setTimeout(function(){ oscillator.stop(); }, 100);
+}
+
+function shuffle(){
+    var divs = document.querySelectorAll('.color');
+    for (var i = divs.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+
+        var t = divs[i].style.backgroundColor;
+        divs[i].style.backgroundColor = divs[j].style.backgroundColor;
+        divs[j].style.backgroundColor = t;
+    }
+
+    if (document.getElementById("soundcheck").checked){
+        beep(Math.random() * 1963 + 130);
+    }
+}
+
+window.onload = function(){
+    setInterval(shuffle, 250);
+};
